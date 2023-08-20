7a2ee4bc7c7fbed0846e2f65d72dbf6dfff2473b
Veeyzu <mveyzha@gmail.com>
2023-08-20 09:45:29 +0700

    gltfthing

--- three/examples/jsm/loaders/GLTFLoader.js
+++ three/examples/jsm/loaders/GLTFLoader.js
@@ -61,7 +61,7 @@ import {
 	Vector3,
 	VectorKeyframeTrack,
 	SRGBColorSpace
-} from '/three/build/three.module.js';
+} from 'three';
 import { toTrianglesDrawMode } from '../utils/BufferGeometryUtils.js';
 
 class GLTFLoader extends Loader {
