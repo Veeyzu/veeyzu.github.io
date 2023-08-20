a7e98b8713db56abd3cbce73d11e13838bb36e0b
Veeyzu <mveyzha@gmail.com>
2023-08-20 10:28:02 +0700

    mobile12

--- main.js
+++ main.js
@@ -52,49 +52,177 @@ const lib = {
 const raycaster = new THREE.Raycaster();
 const pointer = new THREE.Vector2();
 
-document.addEventListener("mousemove", function (event) { 
-    var rect = renderer.domElement.getBoundingClientRect();
-    pointer.x = ( ( event.clientX - rect.left ) / ( rect.width - rect.left ) ) * 2 - 1;
-    pointer.y = - ( ( event.clientY - rect.top ) / ( rect.bottom - rect.top) ) * 2 + 1;
-   
-   
-});
+// mouse event vars
+let isSwiping = false;
+const delta = 1.5;
+let sogliaMove = 0;
+let firstTouch = true;
+let firstTime = true;
+// device detection
+let isMobile = false;
 
-function onMobileTouchStart(event){
-  event.preventDefault();
-  pointer.x = ( event.targetTouches[0].pageX / window.innerWidth) * 2 - 1;
-  pointer.y = -( event.targetTouches[0].pageY / window.innerHeight) * 2 + 1;
+if (
+  /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(
+    navigator.userAgent,
+  )
+  || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
+    navigator.userAgent.substr(0, 4),
+  )
+) {
+  isMobile = true;
 }
 
-document.addEventListener('touchstart', onMobileTouchStart, false);
+//pointer event works better than touch event
+
+  if (isMobile) {
+    document.addEventListener('pointerdown', (event) => {
+      firstTouch = true; 
+      isSwiping = false;
+
+      pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+      pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+      raycaster.setFromCamera( pointer, camera );
+      const intersects = raycaster.intersectObjects( scene.children );
+    
+      if (intersects.length >= 1 ) {
+        if (intersects[0].object.parent.isGroup) {
+          
+          for (let i = 0; i < intersects[0].object.parent.children.length; i++){
+            const color = new THREE.Color("rgb(178, 72, 255)");
+            console.log(intersects[0].object.parent.children[i]);
+    
+            intersects[0].object.parent.children[i].material.emissive = color
+            rotate = Math.floor(Math.random() * 10)-5;
+            document.getElementById("ObjectName").innerText = intersects[0].object.parent.name
+    
+            document.getElementById("Description").innerText = lib[intersects[0].object.parent.name]
+    
+            newIndicator = 0;
+            console.log(newIndicator);
+          }
+    
+          ChosenId = intersects[0].object.parent.id; 
+          //console.log(intersects[0].object);
+        }
+        
+      } 
+
+    });
+    document.addEventListener('pointermove', (event) => {
+      console.log("owo");
+      if (firstTouch) {
+        pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+        pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
+        
+        firstTouch = false;
+      } else {
+        const diffX = Math.abs(event.pageX - pointer.X);
+        const diffY = Math.abs(event.pageY - pointer.Y);
+        if (diffX < delta && diffY < delta && sogliaMove > 2) {
+          // sogliaMove>2 means 2 frame still when isSwiping is true
+          onDocumentTouchClick(event); // for iOS  
+        }
+      }
+      isSwiping = true; 
+    });
+    document.addEventListener('pointerup', (event) => {
+      const diffX = Math.abs(event.pageX - pointer.X);
+      const diffY = Math.abs(event.pageY - pointer.Y);
+      if (diffX < delta && diffY < delta) {
+        onDocumentMouseClick(event); // Android old: is better desktop solution
+      }
+      firstTouch = true;
+    });
+  } else {
+//desktop behavior
+document.addEventListener('pointerdown', (event) => {
+      isSwiping = false;
+      pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+      pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+      raycaster.setFromCamera( pointer, camera );
+      const intersects = raycaster.intersectObjects( scene.children );
+    
+      if (intersects.length >= 1 ) {
+        if (intersects[0].object.parent.isGroup) {
+          
+          for (let i = 0; i < intersects[0].object.parent.children.length; i++){
+            const color = new THREE.Color("rgb(178, 72, 255)");
+            console.log(intersects[0].object.parent.children[i]);
+    
+            intersects[0].object.parent.children[i].material.emissive = color
+            rotate = Math.floor(Math.random() * 10)-5;
+            document.getElementById("ObjectName").innerText = intersects[0].object.parent.name
+    
+            document.getElementById("Description").innerText = lib[intersects[0].object.parent.name]
+    
+            newIndicator = 0;
+            console.log(newIndicator);
+          }
+    
+          ChosenId = intersects[0].object.parent.id; 
+          //console.log(intersects[0].object);
+        }
+        
+      } 
+
+    });
+    document.addEventListener('pointermove', (event) => {
+      isSwiping = true;
+      pointer.x = (event.pageX / window.innerWidth) * 2 - 1;
+      pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
+      console.log(pointer);
+    });
+
+    document.addEventListener('pointerup', (event) => {
+      const diffX = Math.abs(event.pageX - pointer.X);
+      const diffY = Math.abs(event.pageY - pointer.Y);
+
+      if (diffX < delta && diffY < delta) {
+        onDocumentMouseClick(event);
+      }
+    });
+  }
+
+function onDocumentTouchClick(event) {
+  event.preventDefault();
+  scene.updateMatrixWorld();
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+  raycaster.setFromCamera(mouse, camera);
+  intersects = raycaster.intersectObject(mesh);
+
+  if (intersects.length > 0 && firstTouch === false) {
+    firstTouch = true; 
+    evaluateRaycast();
+  }
+}
+
+function onDocumentMouseClick(event) {
+  event.preventDefault();
+  if (!isSwiping) {
+    scene.updateMatrixWorld();
+
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+    raycaster.setFromCamera(mouse, camera);
+    intersects = raycaster.intersectObject(mesh, true);
+
+    if (intersects.length > 0) {
+      evaluateRaycast(); //it continues
+    }
+  }
+  isSwiping = false;
+}
+
+
 
 document.addEventListener("click", function (event) { 
   //window.alert("urmom");
-  raycaster.setFromCamera( pointer, camera );
-  const intersects = raycaster.intersectObjects( scene.children );
-
-  if (intersects.length >= 1 ) {
-    if (intersects[0].object.parent.isGroup) {
-      
-      for (let i = 0; i < intersects[0].object.parent.children.length; i++){
-        const color = new THREE.Color("rgb(178, 72, 255)");
-        console.log(intersects[0].object.parent.children[i]);
-
-        intersects[0].object.parent.children[i].material.emissive = color
-        rotate = Math.floor(Math.random() * 10)-5;
-        document.getElementById("ObjectName").innerText = intersects[0].object.parent.name
-
-        document.getElementById("Description").innerText = lib[intersects[0].object.parent.name]
-
-        newIndicator = 0;
-        console.log(newIndicator);
-      }
-
-      ChosenId = intersects[0].object.parent.id; 
-      //console.log(intersects[0].object);
-    }
-    
-  } 
+  
 
  
 });
